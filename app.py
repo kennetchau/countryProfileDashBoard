42b78c1f97fdee829302cf2158031c027447147b
kenetxk <kennetchau@gmail.com>
2023-11-26 20:16:44 -0500

    Trying CSS Style sheet and changing table to graph

--- app.py
+++ app.py
@@ -6,17 +6,47 @@
 
 ## import modules
 from dash import Dash, html, dash_table, dcc
+import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv("data/WorldPop.csv")
+df = pd.read_csv("data/WorldPop.csv", index_col = 0)
+df2 = df.loc[(df['Population, density and surface area'] == "Total, all countries or areas") & (df['Series'] == "Population mid-year estimates (millions)")]
 
 ## initiate the app
-app = Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+colors = {
+    'textcolor': '#777777'
+}
+
+## Create a function to make graphs
+def linePlot(dataFrame, xValue:str, yValue:str, titleName:str,xAlias=None, yAlias=None):
+    fig = px.line(dataFrame, x=xValue, y=yValue, title=titleName)
+    if xAlias != None:
+        fig.update_layout(xaxis_title = xAlias)
+    if yAlias != None:
+        fig.update_layout(yaxis_title = yAlias)
+    return fig
+
 
 ## App layout
-app.layout = html.Div([
-    html.Div(children = "Country data no modification"),
-    dash_table.DataTable(data = df.to_dict('records'), page_size = 10),
+app.layout = html.Div(children=[
+    html.H1(
+        children = "World Population",
+        style = {
+            'textAlign': 'center',
+            'color': colors['textcolor']
+        }
+    ),
+    html.Div(
+        children = [
+            dcc.Graph(
+                id = "World Population",
+                figure = linePlot(df2, "Year", "Value", titleName = "World Population overtime" ,yAlias="Population (millions)")
+            )
+        ]
+    )
 ])
 
 # Run the app
