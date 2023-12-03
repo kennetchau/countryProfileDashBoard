a5809cc9d8ed67518949df4b58898c305a62210a
Kenneth <kenneth@Kenneths-Mac-mini.local>
2023-12-03 03:39:48 -0500

    Adding style sheet

--- app.py
+++ app.py
@@ -18,17 +18,14 @@ dfCode = df[['Series Code','Series Name']].drop_duplicates()
 ## initiate the app
 app = Dash(__name__)
 
-colors = {
-    'textcolor': '#777777'
-}
 
 ## App layout
 app.layout = html.Div(children=[
     html.H1(
         children = "Country Data",
         style = {
-            'textAlign': 'center',
-            'color': colors['textcolor']
+            'textAlign': 'left',
+            'margin-bottom': '10px'
         }
     ),
     html.Div(
@@ -38,7 +35,7 @@ app.layout = html.Div(children=[
                 df['Country Name'].unique(), 
                 searchable = True, 
                 multi = True, 
-                id = 'country-selector'
+                id = 'country-selector',
             ),
             # Add a slider to select the year
             dcc.Slider(
@@ -51,7 +48,7 @@ app.layout = html.Div(children=[
             ),
             # Add a bar chart to compare data
             dcc.Graph(id = "WorldPopulation_Graph",),
-        ]
+        ],
     )
 ])
 
@@ -62,7 +59,10 @@ app.layout = html.Div(children=[
     Input('country-selector','value')
 )
 def update_figure(selected_year, selected_country):
-    filtered_df = df[(df['Series Code'] == "NY.GDP.MKTP.KD") & (df['Year'] == selected_year) & np.isin(df,selected_country).any(axis = 1)].sort_values(by='value', ascending = False)
+    condition1 = df['Series Code'] == "NY.GDP.MKTP.KD"
+    condition2 = df['Year'] == selected_year
+    condition3 = np.isin(df,selected_country).any(axis = 1)
+    filtered_df = df[condition1 & condition2 & condition3].sort_values(by='value', ascending = False)
     fig = px.bar(filtered_df, x = 'value', y="Country Name", orientation='h', labels = {'value':"GDP (constant 2015 US$)"})
     fig.update_layout(transition_duration = 500)
     return fig
